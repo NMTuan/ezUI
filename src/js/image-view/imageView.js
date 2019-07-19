4bb6ee99b0861f652d5b2ad2071b0231a93b3b2c
NMTuan <nmtuan@qq.com>
2019-07-19 15:11:05 +0800

    增加图片旋转，缩放

--- src/js/image-view/imageView.js
+++ src/js/image-view/imageView.js
@@ -13,8 +13,13 @@ var imageView = {
     windowTpl: function () {
         var el = $('<div>').attr('class', 'ez image-view');
         var html = '' +
-            // '<div class="ez image-view-head"></div>' +
-            // '<div class="ez image-view-body"></div>' +
+            '<i class="ez image-view-close image-view-icon remixicon-close-line"></i>' +
+            '<i class="ez image-view-rotate-left image-view-icon remixicon-anticlockwise-line"></i>' +
+            '<i class="ez image-view-rotate-right image-view-icon remixicon-clockwise-line"></i>' +
+            '<i class="ez image-view-prev image-view-icon remixicon-skip-back-line"></i>' +
+            '<i class="ez image-view-next image-view-icon remixicon-skip-forward-line"></i>' +
+            '<div class="ez image-view-head"></div>' +
+            '<div class="ez image-view-body"></div>' +
             // '<div class="ez image-view-foot"></div>' +
             '';
         el.append(html);
@@ -43,13 +48,16 @@ var imageView = {
     //创建窗口
     createView: function (data, params) {
         var el = imageView.windowTpl();
-        el.css({zIndex: params.zIndex+1});
+        el.css({zIndex: params.zIndex + 1});
         $('body').append(el);
         imageView.createImage(data[params.index].src, function (img) {
-            el.append(img);
+            el.find('.image-view-body').append(img);
             imageView.imageResize(img, params);
         });
         return el;
+    },
+    closeView: function (el) {
+        el.remove();
     },
     //重置大小
     imageResize: function (img, params) {
@@ -61,23 +69,68 @@ var imageView = {
             img.width = params.width;
         }
         $(img).data('width', img.width);
-        $(img).data('zoom', 0);
         // $(img).parent().css({
         //     marginTop: -$(img).height()/2,
         //     marginLeft: -$(img).width()/2
         // });
     },
-    //缩放
-    imageZoom: function (img, whellDir) {
-        img = $(img);
-        whellDir = whellDir || 0;
-        var step = 0.1; //缩放倍数
-        if (whellDir < 0) {   //向下滚动
-            step = 0 - step;
+    //【scale缩放x=a,y=d】
+    scale: function (obj, step) {
+        var scale = obj.data('scale') || 1;
+        scale = parseFloat(scale);
+        scale = scale + parseFloat(step)
+        if (scale <= 0) {
+            scale = 0.1;
         }
-        var zoom = img.data('zoom') + step;
-        img.data('zoom', zoom);
-        img.width(img.data('width') + img.data('width') * zoom);
+        obj.data('scale', scale.toFixed(1));
+        imageView.transform(obj);
+    },
+    //【rotate旋转】
+    rotate: function (obj, dir) {
+        var rotate = obj.data('rotate') || 0;
+        rotate = parseInt(rotate);
+        if (dir === 'right') {
+            rotate = rotate + 90;
+        } else {
+            rotate = rotate - 90;
+        }
+        rotate = rotate % 360;
+        obj.data('rotate', rotate);
+        imageView.transform(obj);
+    },
+    //计算transform值
+    transform: function (obj) {
+        var scale = obj.data('scale') || 1;
+        var scaleArr = [scale, 0, 0, scale, 0, 0]; //缩放矩阵
+        var rotate = obj.data('rotate') || 0;
+        var r = Math.PI / 180 * rotate; //半径
+        var sin = Math.sin(r);
+        var cos = Math.cos(r);
+        var rotateArr = [cos, sin, -sin, cos, 0, 0];   //旋转矩阵
+        var matrix = [];    //最终矩阵
+        matrix.push(scaleArr[0] * rotateArr[0] + scaleArr[2] * rotateArr[1]);
+        matrix.push(scaleArr[1] * rotateArr[0] + scaleArr[3] * rotateArr[1]);
+        matrix.push(scaleArr[0] * rotateArr[2] + scaleArr[2] * rotateArr[3]);
+        matrix.push(scaleArr[1] * rotateArr[2] + scaleArr[3] * rotateArr[3]);
+        matrix.push(scaleArr[0] * rotateArr[4] + scaleArr[2] * rotateArr[5]);
+        matrix.push(scaleArr[1] * rotateArr[4] + scaleArr[3] * rotateArr[5]);
+        imageView.setMatrix(obj, matrix);
+    },
+    //取2d变形的值,【scale缩放x=a,y=d】【translate平移x=e,y=f】【rotate旋转】
+    // getMatrix: function (obj) {
+    //     var matrix = obj.css('transform');
+    //     $.log(matrix);
+    //     if (matrix === 'none') {
+    //         return ["1", "0", "0", "1", "0", "0"];
+    //     }
+    //     matrix = matrix.split(/matrix\((.*?)\)/)[1];
+    //     matrix = matrix.split(', ');
+    //     return matrix;
+    // },
+    setMatrix: function (obj, matrix) {
+        matrix = matrix.join(',');
+        matrix = 'matrix(' + matrix + ')';
+        obj.css('transform', matrix);
     },
     //类
     ImageView: function (data, params) {
@@ -88,19 +141,48 @@ var imageView = {
         if (data[params.index] && data[params.index].src) {
             var el = imageView.createView(data, params);
             //滚动缩放
-            el.mousewheel(function (e) {
+            el.find('img').mousewheel(function (e) {
                 e.preventDefault();
                 e.stopPropagation();
-                imageView.imageZoom(el.find('img'), e.deltaY);
+                imageView.scale($(this), e.deltaY > 0 ? 0.1 : -0.1);
             });
-            //拖拽
+            //窗口拖拽
             el.draggabilly({
-
+                handle: '.image-view-head'
+            });
+            //图片拖拽
+            el.find('.image-view-body').draggabilly({
+                contrainment: true
             });
             el.draggabilly('setPosition', 100, 100);    //初始位置
-            el.on('pointerDown', function () {
+            el.on('mousedown pointerDown', function () {    //鼠标按下，调整当前窗口层级。
                 $('.image-view').css('z-index', params.zIndex);
-                $(this).css('z-index', params.zIndex+1);
+                $(this).css('z-index', params.zIndex + 1);
+            });
+            // el.on('pointerDown', function () {
+            //     $('.image-view').css('z-index', params.zIndex);
+            //     $(this).css('z-index', params.zIndex+1);
+            // });
+
+            //关闭
+            el.find('.image-view-close').on('click', function () {
+                imageView.closeView(el);
+            });
+            //旋转
+            el.find('.image-view-rotate-right').on('click', function () {
+                imageView.rotate(el.find('img'), 'right');
+            });
+            el.find('.image-view-rotate-left').on('click', function () {
+                imageView.rotate(el.find('img'), 'left');
+            });
+            //翻页
+            el.find('.image-view-next').on('click', function () {
+                imageView.createImage(data[params.index + 1].src, function (img) {
+                    el.find('img').remove();
+                    el.find('.image-view-body').append(img);
+                    el.find('.image-view-body').draggabilly('setPosition', 0, 0);
+                    imageView.imageResize(img, params);
+                });
             });
         }
     }
