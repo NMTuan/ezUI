61dd0eafd2a73a7ba5c9f0279658481137a6e411
NMTuan <nmtuan@qq.com>
2019-09-16 09:46:03 +0800

    更新

--- dist/js/ezUI.js
+++ dist/js/ezUI.js
@@ -14008,8 +14008,10 @@ ez.tree = require('./tree/tree'); //树结构
 ez.watermark = require('./watermark/watermark'); //水印
 
 ez.textarea = require('./form/textarea'); //文本域
-}).call(this,require("XJF/FV"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_200683c4.js","/")
-},{"./audioPlayer/audioPlay":14,"./fixedContainer/fixedContainer":16,"./form/player":17,"./form/select":18,"./form/textarea":19,"./form/upload":20,"./headlines/headlines":21,"./iframeTabs/iframeTabs":22,"./imageView/imageView":23,"./log/log":24,"./menuTree/menuTree":25,"./msg/msg":26,"./renderHeight/renderHeight":28,"./role/role":29,"./scrollWheel/scrollWheel":30,"./subNav/subNav":31,"./tabs/tabs":32,"./tree/tree":33,"./watermark/watermark":34,"XJF/FV":7,"buffer":6}],16:[function(require,module,exports){
+
+ez.tableList = require('./table/list');
+}).call(this,require("XJF/FV"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_24ead64d.js","/")
+},{"./audioPlayer/audioPlay":14,"./fixedContainer/fixedContainer":16,"./form/player":17,"./form/select":18,"./form/textarea":19,"./form/upload":20,"./headlines/headlines":21,"./iframeTabs/iframeTabs":22,"./imageView/imageView":23,"./log/log":24,"./menuTree/menuTree":25,"./msg/msg":26,"./renderHeight/renderHeight":28,"./role/role":29,"./scrollWheel/scrollWheel":30,"./subNav/subNav":31,"./table/list":32,"./tabs/tabs":33,"./tree/tree":34,"./watermark/watermark":35,"XJF/FV":7,"buffer":6}],16:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 "use strict";
 
@@ -14512,7 +14514,7 @@ $.fn.extend({
 });
 module.exports = select.Select;
 }).call(this,require("XJF/FV"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/form\\select.js","/form")
-},{"../tree/tree":33,"XJF/FV":7,"buffer":6}],19:[function(require,module,exports){
+},{"../tree/tree":34,"XJF/FV":7,"buffer":6}],19:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 "use strict";
 
@@ -16135,6 +16137,264 @@ module.exports = _subNav.subNav;
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 "use strict";
 
+var _list = {
+  defaults: {
+    data: {
+      header: [],
+      body: []
+    },
+    sort: [],
+    tableClass: ['ez-table-list-border', 'ez-table-list-line', 'ez-table-list-vline', 'ez-table-list-hover', 'ez-table-list-full', 'ez-table-list-stripe'],
+    multiple: false,
+    //多选
+    move: false //列移动
+
+  },
+  list: function list(els, params) {
+    $.each(els, function () {
+      new _list.List($(this), params);
+    });
+  },
+  List: function List(el, params) {
+    var s = this;
+    s.el = el;
+    s.params = $.extend(true, {}, _list.defaults, params);
+
+    _list.renderTable.call(s);
+
+    _list.events.call(s);
+  },
+  events: function events() {
+    var s = this; //点击选中行(单选)
+
+    s.el.on('click', '.ez-table-list-row', function () {
+      console.log('click');
+
+      _list.rowSelected(this);
+
+      _list.rowUnselected($(this).siblings('.ez-table-list-active'));
+
+      $(this).addClass('ez-table-list-active').siblings().removeClass('ez-table-list-active').find('input').prop('checked', false);
+      $(this).find('input').prop('checked', true);
+    }); //勾选(可多选)
+
+    s.el.on('click', 'input', function (e) {
+      e.stopPropagation();
+
+      _list.rowToggleSelected($(this).closest('.ez-table-list-row'));
+    }); //防止意外勾选, 扩大勾选热区
+
+    s.el.on('click', '.ez-table-list-field-checkbox', function (e) {
+      e.stopPropagation();
+
+      _list.rowToggleSelected($(this).closest('.ez-table-list-row'));
+    }); //选项
+
+    s.el.on('click', '.ez-table-list-field-option', function (e) {
+      var el = $('<div>').addClass('ez-table-list'); //.append($('<div>').addClass('ez-table-list'));
+
+      var options = {
+        data: {
+          header: [{
+            field: 'id',
+            title: '编号'
+          }, {
+            field: 'col',
+            title: '列'
+          }],
+          body: [{
+            id: 'id',
+            col: '编号'
+          }, {
+            id: 'code',
+            col: '服务号'
+          }, {
+            id: 'company',
+            col: '所属企业'
+          }, {
+            id: 'agent',
+            col: '所属机构'
+          }, {
+            id: 'site',
+            col: '归属落地'
+          }, {
+            id: 'address',
+            col: '服务器地址'
+          }]
+        },
+        tableClass: ['ez-table-list-border', 'ez-table-list-line', 'ez-table-list-vline', 'ez-table-list-hover', 'ez-table-list-full', 'ez-table-list-sm']
+      };
+      $('body').append(el);
+      new _list.List(el, options);
+      console.log(el);
+      layer.open({
+        type: 1,
+        title: '表格配置',
+        content: el.first(),
+        btn: ['保存']
+      });
+    });
+  },
+  //渲染表格
+  renderTable: function renderTable() {
+    var s = this;
+    var table = $('<div>').addClass('ez-table-list-table').addClass(s.params.tableClass.join(' '));
+
+    var header = _list.renderHeader.call(s);
+
+    var body = _list.renderBody.call(s);
+
+    table.append(header).append(body);
+    s.el.append(table);
+  },
+  //渲染表头
+  renderHeader: function renderHeader() {
+    var s = this;
+    var html = $('<div>').addClass('ez-table-list-head');
+    var row = $('<div>').addClass('ez-table-list-row');
+    var dragBtn = $('<i>').addClass('remixicon-more-2-line'); //多选框
+
+    if (s.params.multiple) {
+      var optionBtn = $('<i>').addClass('remixicon-list-settings-line');
+
+      var cell = _list.renderCell('option', true);
+
+      cell.css('width', '1px');
+      cell.html(optionBtn);
+      row.append(cell);
+    } //如果有顺序配置, 则按配置执行, 否则输出全字段
+
+
+    if (s.params.sort.length > 0) {
+      $.each(s.params.sort, function (i, field) {
+        $.each(s.params.data.header, function (i, item) {
+          if (item.field !== field) {
+            return;
+          }
+
+          var cell = _list.renderCell(item.field, true);
+
+          cell.html(item.title);
+
+          if (s.params.move) {
+            cell.prepend(dragBtn.clone());
+          }
+
+          row.append(cell);
+        });
+      });
+    } else {
+      $.each(s.params.data.header, function (i, item) {
+        var cell = _list.renderCell(item.field, true);
+
+        cell.html(item.title);
+
+        if (s.params.move) {
+          cell.prepend(dragBtn.clone());
+        }
+
+        row.append(cell);
+      });
+    }
+
+    html.append(row);
+    return html;
+  },
+  //渲染表格主体
+  renderBody: function renderBody() {
+    var s = this;
+    var html = $('<div>').addClass('ez-table-list-body');
+    $.each(s.params.data.body, function (i, item) {
+      var row = _list.renderRow.call(s, item);
+
+      html.append(row);
+    });
+    return html;
+  },
+  //渲染行, data单元格数据数组
+  renderRow: function renderRow(data) {
+    var s = this;
+    var html = $('<div>').addClass('ez-table-list-row');
+
+    if (s.params.multiple) {
+      var checkbox = $('<input>').attr({
+        type: 'checkbox',
+        name: '',
+        value: data.id
+      });
+
+      var cell = _list.renderCell('checkbox', true);
+
+      cell.append(checkbox);
+      html.append(cell);
+    } //如果有顺序配置, 则按配置执行, 否则输出全字段
+
+
+    if (s.params.sort.length > 0) {
+      $.each(s.params.sort, function (i, field) {
+        $.each(data, function (key, value) {
+          if (key !== field) {
+            return;
+          }
+
+          var cell = _list.renderCell(key);
+
+          cell.html(value);
+          html.append(cell);
+        });
+      });
+    } else {
+      $.each(data, function (key, value) {
+        var cell = _list.renderCell(key);
+
+        cell.html(value);
+        html.append(cell);
+      });
+    }
+
+    return html;
+  },
+  //渲染单元格, field字段, th是否为th
+  renderCell: function renderCell(field, th) {
+    var cls = [];
+    cls.push(th ? 'ez-table-list-th' : 'ez-table-list-td');
+
+    if (field) {
+      cls.push('ez-table-list-field-' + field);
+    }
+
+    return $('<div>').addClass(cls.join(' '));
+  },
+  //选中当前行
+  rowSelected: function rowSelected(row) {
+    $(row).addClass('ez-table-list-active').find('input').prop('checked', true);
+  },
+  //取消当前行
+  rowUnselected: function rowUnselected(row) {
+    $(row).removeClass('ez-table-list-active').find('input').prop('checked', false);
+  },
+  //切换选中状态
+  rowToggleSelected: function rowToggleSelected(row) {
+    if ($(row).hasClass('ez-table-list-active')) {
+      _list.rowUnselected(row);
+    } else {
+      _list.rowSelected(row);
+    }
+  }
+};
+$.fn.extend({
+  ez_table_list: function ez_table_list(params) {
+    _list.list(this, params);
+
+    return this;
+  }
+});
+module.exports = _list.list;
+}).call(this,require("XJF/FV"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/table\\list.js","/table")
+},{"XJF/FV":7,"buffer":6}],33:[function(require,module,exports){
+(function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
+"use strict";
+
 var _tabs = {
   defaults: {
     currentClass: 'current',
@@ -16200,7 +16460,7 @@ $.fn.extend({
 });
 module.exports = _tabs.tabs;
 }).call(this,require("XJF/FV"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/tabs\\tabs.js","/tabs")
-},{"XJF/FV":7,"buffer":6}],33:[function(require,module,exports){
+},{"XJF/FV":7,"buffer":6}],34:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 "use strict";
 
@@ -16645,7 +16905,7 @@ $.fn.extend({
 });
 module.exports = tree.Tree;
 }).call(this,require("XJF/FV"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/tree\\tree.js","/tree")
-},{"../random/random":27,"XJF/FV":7,"buffer":6}],34:[function(require,module,exports){
+},{"../random/random":27,"XJF/FV":7,"buffer":6}],35:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 "use strict";
 
