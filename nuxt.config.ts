0fdc9fec772366e6bd61990f97f0e8bbaeb88e5d
NMTuan <nmtuan@qq.com>
2023-04-06 17:05:31 +0800

    chore: 引入 style reset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
+/*
+ * @Author: NMTuan
+ * @Email: NMTuan@qq.com
+ * @Date: 2023-04-06 11:30:37
+ * @LastEditTime: 2023-04-06 17:03:56
+ * @LastEditors: NMTuan
+ * @Description:
+ * @FilePath: \ezUI\nuxt.config.ts
+ */
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    css: ['@unocss/reset/tailwind.css'],
     modules: ['@unocss/nuxt']
 })
